0c7003b8351e9c93507d91c29fad597fcb011638
RobertDoms <robertdoms90@gmail.com>
2023-01-03 11:34:56 +0200

    Update ContactInput.jsx

--- src/components/ContactInput/ContactInput.jsx
+++ src/components/ContactInput/ContactInput.jsx
@@ -27,3 +27,26 @@ ContactInputSection.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
 export default ContactInputSection;
+
+const Section = styled.section`
+  margin: 20px auto;
+  padding: 10px;
+  text-align: center;
+  width: 300px;
+  border: 2px solid orange;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  & label {
+    max-width: 200px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  & button {
+    max-width: 120px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
