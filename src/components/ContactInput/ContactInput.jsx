eddca6eb5550b05670344d9bcb3146a154251dd6
RobertDoms <robertdoms90@gmail.com>
2023-01-03 20:30:52 +0200

    Update ContactInput.jsx

--- src/components/ContactInput/ContactInput.jsx
+++ src/components/ContactInput/ContactInput.jsx
@@ -22,6 +22,42 @@ class ContactInputSection extends Component {
   reset = () => {
     this.setState({ name: '', number: '' });
   };
+  render() {
+    return (
+      <Section>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+
+          <label>
+            Number
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              onChange={this.handleChange}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </Form>
+      </Section>
+    );
+  }
 }
 ContactInputSection.propTypes = {
   onSubmit: PropTypes.func.isRequired,
